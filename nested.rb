bae70c8c5f91e2c4186c8309ba68145dbe884292
Xiaoyi Liu <lxy878@gmail.com>
2020-03-06 16:01:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,7 +128,6 @@ def adding_to_dennis
         }
      }
 		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-		 puts programmer_hash[:dennis_ritchie][:languages]
 
 		 return programmer_hash
 end
