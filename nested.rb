107b5e30181bf05851aa98cbefd82d99efe3e4b7
Xiaoyi Liu <lxy878@gmail.com>
2020-03-06 15:54:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 return programmer_hash[:dennis_ritchie][:languages]
+		 return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -82,7 +82,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+		 programmer_hash[:yukihiro_matsumoto]={}
+		 programmer_hash[:yukihiro_matsumoto][:known_for]="Ruby"
+		 programmer_hash[:yukihiro_matsumoto][:languages]=["LISP", "C"]
 
 end
 
