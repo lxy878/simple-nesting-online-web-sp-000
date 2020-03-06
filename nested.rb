a188d61b46f3e1974b456e17cd3ec51f4b7fc6b4
Xiaoyi Liu <lxy878@gmail.com>
2020-03-06 15:58:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,7 +106,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
+		 programmer_hash[:alan_kay][:known_for] = "GUI"
+		 programmer_hash
 
 end
 
@@ -126,5 +127,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages] << "Assenbly"
 
+		 return programmer_hash
 end
