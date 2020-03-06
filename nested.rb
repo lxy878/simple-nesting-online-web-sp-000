fddb4a9d0a568d17ea769feb7187fcbaa22a0409
Xiaoyi Liu <lxy878@gmail.com>
2020-03-06 16:01:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,8 +127,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 languages = programmer_hash[:dennis_ritchie][:languages]
-		 languages << "Assembly"
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 		 puts languages.size
 
 		 return programmer_hash
